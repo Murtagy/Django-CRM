31413d99b728b26919299fecf2bc8897f1bf4d53
Murtagy <mrabkevich.gmail.com>
2019-12-28 14:25:42 +0300

    Related action -> child actions

--- crm/views/activities_views.py
+++ crm/views/activities_views.py
@@ -101,12 +101,11 @@ class DealDetailView(DetailView):
     template_name = "crm/deals/deal_detail.html"
 
     @property
-    def get_related_actions(self):
+    def get_child_actions(self):
         queryset = Action.objects.filter(deal_fk=self.object.id)
         paginator = Paginator(queryset,5)
         page = self.request.GET.get('page')
         actions = queryset.get_page(page)
-        print(111111111111111)
         return actions
 
 class DealAddView(PermissionRequiredMixin, CreateView):
